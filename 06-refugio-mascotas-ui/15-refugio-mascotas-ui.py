5a8e71f527085ae96f12134cf3fd570394a4659a
Juitomg <juitomg@gmail.com>
2025-01-07 20:47:10 +0100

    Continuado del ejercicio en UI

--- 06-refugio-mascotas-ui/15-refugio-mascotas-ui.py
+++ 06-refugio-mascotas-ui/15-refugio-mascotas-ui.py
@@ -39,6 +39,37 @@ class RegistroMascotasGUI:
         tk.Label(self.root,text="Tipo:").grid(row=1,column=0,padx=10,pady=5)
         self.entry_tipo = tk.Entry(self.root)
         self.entry_tipo.grid(row=1,column=1,padx=10,pady=5)
+
+        tk.Label(self.root,test="Edad:").grid(row=2,column=0,padx=10,pady=5)
+        self.entry_edad = tk.Entry(self.root)
+        self.entry_edad.grid(row=2,column=1,padx=10,pady=5)
+
+        tk.Label(self.root,text="Estado:").grid(row=3,column=0,padx=10,pady=5)
+        self.entry_estado = tk.Entry(self.root)
+        self.entry_estado.grid(row=3, column=1,padx=10,pady=5)
+
+        tk.Button(self.root, text="Registrar Mascota", command=self.agregar_mascota).grid(row=4,column=0,columnspan=2,pady=10)
+        tk.Button(self.root, text="Listar Mascotas", command=self.listar_mascotas).grid(row=5,column=0,columnspan=2,pady=10)
+
+    def agregar_mascota(self):
+        nombre = self.entry_nombre.get()
+        tipo = self.entry_tipo()
+        try:
+            edad = int(self.entry_edad.get())
+        except ValueError:
+            messagebox.showerror("Error", "La edad debe ser un número. ")
+            return
+        estado = self.entry_estado.get()
+
+        if estado not in ['En adopcion','Adoptado']:
+            messagebox.showerror("Error", "El estado debe ser 'En adopcion' o 'Adoptado'.")
+            return
+
+        self.cursor.execute('''
+            INSERT INTO mascotas (nombre, tipo, edad, estado)
+            VALUES (?,?,?,?)
+        ''',(nombre,tipo,edad,estado))
+        self.conn.commit()
         
-                
+                        
         
