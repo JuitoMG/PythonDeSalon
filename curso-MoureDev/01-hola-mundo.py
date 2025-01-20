f189ea8e14a4921e4c16570d4263322224521aa7
Juitomg <juitomg@gmail.com>
2025-01-20 20:50:13 +0100

    Agregados ejercicios

---
+++ curso-MoureDev/01-hola-mundo.py
@@ -0,0 +1,53 @@
+# 1. Imprime "Hola Mundo!" por consola
+print("Hola Mundo!")
+
+# 2. Escribe un comentario de una sola línea explicando qué hace el código anterior
+
+# El código anterior imprime una cadena de texto mostrándola en la consola
+
+# 3. Imprime tu nombre y edad en la misma línea utilizando la función print
+
+print("Juan, 33 años")
+
+# 4. Usa la función type() para imprimir el tipo de dato de una cadena de texto, un 
+# entero y un decimal
+
+cadena = "cadena"
+entero = 98
+decimal = 9.8
+
+print(type(cadena))
+print(type(entero))
+print(type(decimal))
+
+# 5. Escribe un comentario en varias líneas  explicando qué son los tipos de dato en Python
+
+'''
+Los tipos de dato en Python es la clasificación de los valores que puede tener una variable, 
+por ejemplo tipo entero, tipo cadena de texto, etc
+'''
+
+# 6. Imprime el resultado de concatenar dos cadenas de texto, por ejemplo "Hola" y "Mundo"
+
+print("Hola"+"Mundo")
+
+# 7. Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma línea.
+
+nombre = "Juan"
+edad = 33
+print(nombre,edad)
+
+# 8. Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
+
+intr_nomb = input("Introduce nombre")
+print("Hola",nombre,"!")
+
+
+# 9. Usa print() para mostrar el resultado de la suma de dos números enteros y el tipo de dato resultante.
+
+suma = 5 + 7       #Suma 5 más 7 y lo guarda en la variable "suma"
+tipo = type(suma)     #Calcula el tipo de dato de la función "suma" y lo guarda en la varialbe "tipo"
+print("Resultado:",suma,". Tipo:",tipo)   #Imprime una cadena de texto concatenando las variables
+
+# 10. Comenta el código del Ejercicio 9, y explica qué hace cada línea usando comentarios de una sola línea.
+#Hecho
