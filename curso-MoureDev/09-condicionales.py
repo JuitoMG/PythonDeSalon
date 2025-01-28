6a0704417611f7511e78d20baa2ec8d16ce22a4e
Juitomg <juitomg@gmail.com>
2025-01-28 01:50:46 +0100

    Terminado Condicionales

--- curso-MoureDev/09-condicionales.py
+++ curso-MoureDev/09-condicionales.py
@@ -49,9 +49,9 @@ if (numero%3) == 0 and (numero%5) == 0:
 
 # 6. Solicita al usuario que ingrese un número y verifica si es par o impar.
 
-numero = input("Ingresa un número para saber si es par o impar")
+numero = int(input("Ingresa un número para saber si es par o impar"))
 
-if numero%2 == 0:
+if (numero%2) == 0:
     print("Es par")
 else:
     print("Es impar")
@@ -60,13 +60,47 @@ else:
 # 7. Escribe un programa que determine si una persona puedevotar en función de su edad(mayor o igual a 18). Si tiene 16 o 17 años, indica 
 # que puede votar con permisoespecial.
 
+edad = 17
+
+if edad > 18:
+    print("Puede votar")
+elif edad >15 and edad <18:
+    print("Puede votar con permiso especial")
+else:
+    print("No puede votar")
+
+
 
 # 8. Crea un programa que solicite una contraseña al usuario y verifique si coincide con una contraseña predefinida.
 # Si no coincide, muestra un mensaje de error.
 
+contrasena_predef = "esmorga"
+
+contrasena = input("Introduce contraseña\n")
+if contrasena == contrasena_predef:
+    print("Contraseña correcta")
+else:
+    print("Contraseña incorrecta")    
 
 # 9. Escribe un programa que determine si un número está entre 10 y 20 (ambos incluidos).
 
+numero = int(input("Introduce otro número"))
+
+if numero >= 10 and numero <= 20:
+    print("Correcto")
+else:
+    print("Fuera de rango")
 
 # 10. Escribe un programa que simule un semáforo:solicita al usuario que ingrese un color(rojo, amarillo,verde) y muestra un mensaje 
 # indicando si debe detenerse, estar alerta o avanzar.
+
+color = input("Ingrese color (rojo,amarillo,verde)\n")
+
+if color == "verde":
+    print("Avance")
+elif color == "amarillo":
+    print("Alerta")
+elif color == "rojo":
+    print("Pare") 
+else:
+    print("Error: Color incorrecto")           
