c6fa3b3ffc65ee28d0b4cc3f152f46bbcd1b7e7a
JuitoMG <juitomg@gmail.com>
2025-02-03 19:24:31 +0100

    Continuacion python clases

--- curso-MoureDev/12-clases.py
+++ curso-MoureDev/12-clases.py
@@ -120,13 +120,68 @@ print("Nota media de ",estudiante.nombre,":",estudiante.media())
 # 7. Crea una clase "BankAccount" con propiedades como "owner" y "balance". Añade métodos para depositar y retirar
 # dinero, asegurándote de que no se pueda retirar más de lo que hay en la cuenta.
 
+class BankAccount:
+    def __init__(self,owner,balance):
+        self.owner = owner
+        self.balance = balance
+
+    def ingeso(self,ingreso):
+        self.ingreso = ingreso
+        self.balance = self.balance + self.ingreso
+        print("Ingresados ",ingreso,"€. Saldo actual:",self.balance,"€")
+
+    def retirada(self,retiro):
+        self.retiro = retiro
+        if retiro <= self.balance:
+            self.balance = self.balance - self.retiro
+            print("Retirados ",retiro,"€. Saldo actual:",self.balance,"€")
+        else:
+            print("Saldo insuficiente")
+
+cliente = BankAccount("Fulano",300.0)
+cliente.ingeso(30)
+cliente.retirada(50)
+cliente.retirada(400)
+
+
 
 # 8. Crea una clase "Point" que represente un punto en elespacio 2D con coordenadas "x" e "y". Añade un método que
 # calcule la distancia entre dos puntos.
 
+import math
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def distancia(self):
+        distancia = self.x - self.y
+        if distancia <0:
+            distancia = -distancia
+        print("La distancia entre ",self.x,"y",self.y,"es:",distancia)
+        
+        
+punto = Point(4,9)
+punto.distancia()        
+
 
 # 9. Crea una clase "Employee" que tenga propiedades como "name", "hourly_wage" (pago por hora) y "hours_worked".
 # Añade un método que calcule el pago total basado en las horas trabajadas y el salario por hora.
+
+class Employee:
+    def __init__(self,name,hourly_wage,hours_worked):
+        self.name = name
+        self.hourlt_wage = hourly_wage
+        self.hours_worked = hours_worked
+
+    def salario(self):
+        salario = self.hourlt_wage * self.hours_worked
+        print("Salario de",self.name,":",self.hours_worked,"horas,",self.hourlt_wage,"€/h. Total:",salario,"€")  
+
+empleado = Employee("Fulano",8,245)
+
+empleado.salario()
 
 
 # 10. Crea una clase "Store" que tenga una propiedad "inventory" (una lista de productos). Añade un método para
