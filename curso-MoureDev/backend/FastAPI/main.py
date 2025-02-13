7520b87b018d2f3dcadf13d87b565b18d3d9bd14
JuitoMG <juitomg@gmail.com>
2025-02-13 18:31:12 +0100

    Creacion users.py

--- curso-MoureDev/backend/FastAPI/main.py
+++ curso-MoureDev/backend/FastAPI/main.py
@@ -1,2 +1,26 @@
 from fastapi import FastAPI
 
+app = FastAPI()
+
+# URL Local: http://127.0.0.1:8000
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
+
+# Desplegar servidor que hemos creado: uvicorn main:app --reaload
+
+@app.get("/url")
+async def url():
+    return {"url":"https://mouredev.com/python" }
+
+    # URL Local: http://127.0.0.1:8000/url
+
+
+# Swagger UI para generar documentación
+
+# O tambien 127.0.0.1/docs o /redoc
+
+#Postman - Cliente para realizar peticiones a servidor local
+
+#ThunderClient Cliente para la API en VS Code
